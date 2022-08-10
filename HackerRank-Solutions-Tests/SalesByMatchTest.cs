13a5698fd2e643a451edd351f6b022aedbf1a87b
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-08-09 23:27:26 -0500

    Add Sales by Match

---
+++ HackerRank-Solutions-Tests/SalesByMatchTest.cs
@@ -0,0 +1,38 @@
+﻿using HackerRank_Solutions;
+
+namespace HackerRank_Solutions_Tests
+{
+    [TestClass]
+    public class SalesByMatchTest
+    {
+        [TestMethod]
+        public void SocketMerchantTest1()
+        {
+            // Arrange
+            var n = 9;
+            var ar = new List<int>() { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            var expectedPairs = 3;
+
+            // Act
+            var actualPairs = SalesByMatch.SockMerchant(n, ar);
+
+            // Assert
+            Assert.AreEqual(expectedPairs, actualPairs);
+        }
+
+        [TestMethod]
+        public void SocketMerchantTest2()
+        {
+            // Arrange
+            var n = 15;
+            var ar = new List<int>() { 6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5 };
+            var expectedPairs = 6;
+
+            // Act
+            var actualPairs = SalesByMatch.SockMerchant(n, ar);
+
+            // Assert
+            Assert.AreEqual(expectedPairs, actualPairs);
+        }
+    }
+}
---
+++ HackerRank-Solutions/SalesByMatch.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank_Solutions
+{
+    public static class SalesByMatch
+    {
+        public static int SockMerchant(int n, List<int> ar)
+        {
+            var hashSet = new HashSet<int>();
+            var pairs = 0;
+
+            for(var i=0; i<n; i++)
+            {
+                if (!hashSet.Contains(ar[i]))
+                {
+                    hashSet.Add(ar[i]);
+                }
+                else
+                {
+                    pairs++;
+                    hashSet.Remove(ar[i]);
+                }
+            }
+            return pairs;
+        }
+    }
+}
