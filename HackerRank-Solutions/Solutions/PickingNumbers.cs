4e0c619b2719fda229d038f73f799a37bb261568
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-08-10 23:09:17 -0500

    Fixing spelling mistake

--- HackerRank-Solutions/Solutions/PickingNumbers.cs
+++ HackerRank-Solutions/Solutions/PickingNumbers.cs
@@ -5,17 +5,17 @@
         public static int PickingNumbersSolution(List<int> a)
         {
             var hashSet = a.ToHashSet();
-            var subArraysLenght = new int[hashSet.Count];
+            var subArraysLengtt = new int[hashSet.Count];
 
             for(var i=0; i<hashSet.Count; i++)
             {
                 var currentNumber = hashSet.ElementAt(i);
                 var equalNumbers = a.FindAll(e => e == currentNumber).Count;
-                subArraysLenght[i] = Math.Max(a.Where(e => e == currentNumber + 1).Count(), 
+                subArraysLengtt[i] = Math.Max(a.Where(e => e == currentNumber + 1).Count(), 
                                                 a.Where(e => e == currentNumber - 1).Count()) + equalNumbers;
             }
 
-            return subArraysLenght.Max();
+            return subArraysLengtt.Max();
         }
     }
 }
