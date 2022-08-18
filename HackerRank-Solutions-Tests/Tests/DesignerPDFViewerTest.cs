524f10190b648fbef8d5aae43f0ec286c3e2120d
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-08-18 12:57:05 -0500

    Adding designer pdf viewer

---
+++ HackerRank-Solutions-Tests/Tests/DesignerPDFViewerTest.cs
@@ -0,0 +1,46 @@
+﻿using HackerRank_Solutions.Solutions;
+
+namespace HackerRank_Solutions_Tests.Tests
+{
+    [TestClass]
+    public class DesignerPDFViewerTest
+    {
+        [TestMethod]
+        public void DesignerPDFViewerTest1()
+        {
+            // Arrange 
+            var h = new List<int> 
+            {
+                1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 
+                5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 
+            };
+            var word = "abc";
+            var expectedHighlightedArea = 9;
+
+            // Act
+            var actualHighlightedArea = DesignerPDFViewer.DesignerPdfViewer(h, word);
+
+            // Assert
+            Assert.AreEqual(expectedHighlightedArea, actualHighlightedArea);
+        }
+
+        [TestMethod]
+        public void DesignerPDFViewerTest2()
+        {
+            // Arrange 
+            var h = new List<int>
+            {
+                1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5,
+                5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7
+            };
+            var word = "zaba";
+            var expectedHighlightedArea = 28;
+
+            // Act
+            var actualHighlightedArea = DesignerPDFViewer.DesignerPdfViewer(h, word);
+
+            // Assert
+            Assert.AreEqual(expectedHighlightedArea, actualHighlightedArea);
+        }
+    }
+}
---
+++ HackerRank-Solutions/Solutions/DesignerPDFViewer.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank_Solutions.Solutions
+{
+    public static class DesignerPDFViewer
+    {
+        public static int DesignerPdfViewer(List<int> h, string word)
+        {
+            var maxHeight = 0;
+            foreach (var c in word.Distinct())
+            {
+                var index = char.ToUpper(c) - 65;
+                if (h[index] > maxHeight)
+                {
+                    maxHeight = h[index];
+                }
+            }
+            return maxHeight * word.Length;
+        }
+    }
+}
