bd140b1d27af78fa32945e299d4f6f446fcd41da
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-09-10 13:12:38 -0500

    Add Utopian Tree

---
+++ HackerRank-Solutions-Tests/Tests/UtopianTreeTest.cs
@@ -0,0 +1,37 @@
+﻿using HackerRank_Solutions.Solutions;
+
+namespace HackerRank_Solutions_Tests.Tests
+{
+    [TestClass]
+    public class UtopianTreeTest
+    {
+        [TestMethod]
+        public void UtopianTreeTest1()
+        {
+            // Arrange
+            var n = 5;
+            var expectedHeight = 14;
+
+            // Act
+            var actualHeight = UtopianTree.UtopianTreeSolution(n);
+
+            // Assert
+            Assert.AreEqual(expectedHeight, actualHeight);
+        }
+
+        [TestMethod]
+        public void UtopianTreeTest2()
+        {
+            // Arrange 
+            var n = 55;
+            var expectedHeight = 536870910;
+
+            // Act
+            var actualHeight = UtopianTree.UtopianTreeSolution(n);
+
+            // Assert 
+            Assert.AreEqual(expectedHeight, actualHeight);
+        }
+
+    }
+}
---
+++ HackerRank-Solutions/Solutions/UtopianTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Solutions.Solutions
+{
+    public static class UtopianTree
+    {
+        public static int UtopianTreeSolution(int n)
+        {
+            var height = 1;
+            for(var i =1; i<=n; i++)
+            {
+                height = i % 2 == 0 ? height += 1 : height *= 2;
+            }
+            return height;
+        }
+    }
+}
