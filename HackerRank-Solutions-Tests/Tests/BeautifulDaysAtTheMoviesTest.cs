0cc99083f386065997676193d3b2156d98b1e916
FranciscoLicon <francisco17@ciencias.unam.mx>
2022-09-18 13:57:55 -0500

    Add Beautiful Days at the Movies

---
+++ HackerRank-Solutions-Tests/Tests/BeautifulDaysAtTheMoviesTest.cs
@@ -0,0 +1,45 @@
+﻿using HackerRank_Solutions.Solutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Solutions_Tests.Tests
+{
+    [TestClass]
+    public class BeautifulDaysAtTheMoviesTest
+    {
+        [TestMethod]
+        public void BeautifulDaysAtTheMoviesTest1()
+        {
+            // Arrange
+            var i = 20;
+            var j = 23;
+            var k = 6;
+            var expectedBeautifulDays = 2;
+
+            // Act
+            var actualBeautifulDays = BeautifulDaysAtTheMovies.BeautifulDays(i, j, k);
+
+            // Assert
+            Assert.AreEqual(expectedBeautifulDays, actualBeautifulDays);
+        }
+
+        [TestMethod]
+        public void BeautifulDaysAtTheMoviesTest2()
+        {
+            // Arrange
+            var i = 1;
+            var j = 123456;
+            var k = 13;
+            var expectedBeautifulDays = 9657;
+
+            // Act
+            var actualBeautifulDays = BeautifulDaysAtTheMovies.BeautifulDays(i, j, k);
+
+            // Assert
+            Assert.AreEqual(expectedBeautifulDays, actualBeautifulDays);
+        }
+    }
+}
---
+++ HackerRank-Solutions/Solutions/BeautifulDaysAtTheMovies.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRank_Solutions.Solutions
+{
+    public static class BeautifulDaysAtTheMovies
+    {
+        public static int BeautifulDays(int i, int j, int k)
+        {
+            var beautifulDays = 0;
+            for(var l=i; l<=j; l++)
+            {
+                if(Math.Abs(l - Reverse(l)) % k == 0)
+                {
+                    beautifulDays++;
+                }
+            }
+            return beautifulDays;
+        }
+
+        private static int Reverse(int n)
+        {
+            var reverse = 0;
+            while(n > 0)
+            {
+                reverse = reverse * 10 + n % 10;
+                n /= 10;
+            }
+            return reverse;
+        }
+    }
+}
